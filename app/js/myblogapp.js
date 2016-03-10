46a9ce5e1c14d255a3e6694fef281fcaa08e7b28
Isidro Sosa <isidro.sosa.p@gmail.com>
2016-03-09 21:05:26 -0600

    Add show list.

--- app/js/myblogapp.js
+++ app/js/myblogapp.js
@@ -1,4 +1,15 @@
 (function() {
   'use strict';
-  var app = angular.module("myBlogApp", []);
+  var app = angular.module("myBlogApp", ["ngRoute"]);
+  app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "../views/partials/list.html",
+        controller: "PostListCtrl as posts"
+      })
+      .when("/posts/:postId", {
+        templateUrl: "../views/partials/show.html",
+        controller: "ShowPostCtrl as post"
+      });
+  }]);
 }());
---
+++ app/js/post/PostListCtrl.js
@@ -0,0 +1,26 @@
+(function() {
+  angular.module("myBlogApp")
+    .controller("PostListCtrl", PostListCtrl);
+
+  function PostListCtrl() {
+    console.log("Hola");
+    var me = this;
+    me.name = "Hola";
+    me.posts = [
+      {
+        "postId": 0,
+        "title": "My title",
+        "content": "<p>Amet, aliquam id diam maecenas ultricies mi eget mauris pharetra.</p> <p>Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus? Lacus, luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo, laoreet. Viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra, magna ac placerat vestibulum!</p>",
+        "createdAt": new Date("01-01-2016"),
+        "author": "Lacus Luctus"
+      },
+      {
+        "postId": 1,
+        "title": "My new title",
+        "content": "<p>Amet, aliquam id diam maecenas ultricies mi eget mauris pharetra.</p> <p>Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus? Lacus, luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo, laoreet. Viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra, magna ac placerat vestibulum!</p>",
+        "createdAt": new Date("02-01-2016"),
+        "author": "Lacus Luctus"
+      }
+    ];
+  };
+}());
