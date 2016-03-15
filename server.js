55a7cea8935248f6b224084756da2490a927621d
Isidro Sosa <isidro.sosa.p@gmail.com>
2016-03-14 18:23:22 -0600

    Delete bodyParser, path, multer and change app.listen.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var express = require('express'),
-    bodyParser = require('body-parser'),
     serverStatic = require('serve-static'),
-    path = require('path'),
-    multer = require('multer'),
     cons = require('consolidate'),
-    port = 9999;
+    port = process.env.PORT || 9999;
 
 var app = express();
+
 app.engine('html', cons.swig);
+
 app.set('views', __dirname + '/app/public/views');
+
 app.set('view engine', 'html');
+
 app.use(serverStatic(__dirname + '/app/public'));
+
 app.get('/', function (req, res) {
   res.render('index');
 });
-app.listen(port);
-console.log('listenig on port ' + port + '...');
+
+app.listen(port, function() {
+  console.log('listenig on port ' + port + '...');
+});
