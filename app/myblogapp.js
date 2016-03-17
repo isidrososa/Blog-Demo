817c197e37ca4c8f7734a272857c053fec0567ca
Isidro Sosa <isidro.sosa.p@gmail.com>
2016-03-17 16:25:06 -0600

    Split routes of myblogapp.js and add default route.

--- app/myblogapp.js
+++ app/myblogapp.js
@@ -1,30 +1,5 @@
 (function() {
   'use strict';
+
   var app = angular.module("myBlogApp", ["ngRoute", "ngResource", "ngCkeditor", "ngSanitize"]);
-
-  app.config(["$routeProvider", function($routeProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "/views/partials/list.html",
-        controller: "ArticlesCtrl as list"
-      })
-      .when("/articles/new", {
-        templateUrl: "/views/partials/create.html",
-        controller: "ArticlesCtrl as create"
-      })
-      .when("/articles/:articleId", {
-        templateUrl: "/views/partials/show.html",
-        controller: "ArticlesCtrl as show"
-      })
-      .when("/articles/:articleId/edit", {
-        templateUrl: "/views/partials/edit.html",
-        controller: "ArticlesCtrl as edit"
-      })
-      .when("/authors/:authorId", {
-        templateUrl: "/views/partials/showByAuthor.html",
-        controller: "AuthorsCtrl as author"
-      })
-    ;
-
-  }]);
 }());
